ea78d00a9830039ce6c8fd4a55e8a38c45d67fbf
Damian Dominik <damian.dominik5@gmail.com>
2023-07-01 20:34:42 +0200

    feat: router added

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import compression from "compression";
 import cors from "cors";
 import mongoose from "mongoose";
 
+import router from "./router";
+
 const app = express();
 
 app.use(
@@ -29,3 +31,5 @@ const MONGO_URL = process.env.MONGO_URL;
 mongoose.Promise = Promise;
 mongoose.connect(MONGO_URL);
 mongoose.connection.on("error", (error: Error) => console.log(error));
+
+app.use("/", router());
